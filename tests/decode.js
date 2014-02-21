0d815bd1a1d33fc73168d8a0bab8c6c745144110
Alexander Olsson <noseglid@gmail.com>
2014-02-21 20:43:47 +0100

    Added large lorem ipsum test

--- tests/decode.js
+++ tests/decode.js
@@ -1,6 +1,7 @@
 var _      = require('underscore');
 var base85 = require('../lib/base85');
 var data   = require('./data').data;
+var fs     = require('fs');
 
 exports.testErrors = function(test)
 {
@@ -29,5 +30,17 @@ exports.testWhiteSpace = function(test)
   test.deepEqual(base85.decode("<~\n @  p \ns7 \n t D .3            ~>"), new Buffer('canumb'));
   test.deepEqual(base85.decode("<~@ps7tD.3        \n    ~>"), new Buffer('canumb'));
   test.deepEqual(base85.decode("<~       @ps7tD.3     \n     ~>"), new Buffer('canumb'));
+  test.done();
+}
+
+exports.testLarge = function(test)
+{
+  var raw     = new Buffer(fs.readFileSync('./tests/loremipsum.raw'));
+  var enc     = new Buffer(fs.readFileSync('./tests/loremipsum.base85'));
+  var decoded = base85.decode(enc);
+  var encoded = base85.encode(raw);
+
+  test.deepEqual(decoded, raw);
+  test.deepEqual(encoded, enc.toString('ascii'));
   test.done();
 }
